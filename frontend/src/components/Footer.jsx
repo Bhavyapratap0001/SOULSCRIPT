c35073a74bd039cee1b6e8bbbe72091890118d1a
Bhavya Pratap Singh <bhavyapratap0707@gmail.com>
2025-12-05 18:36:42 +0530

    changes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
       <p>
-        &copy; {new Date().getFullYear()} SoulScript | 
+        &copy; {new Date().getFullYear()} SoulScript 
       </p>
     </footer>
   );
